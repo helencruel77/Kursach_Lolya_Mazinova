504440647133c40a39ce3d4dd2a6144b02f513b7
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-29 19:18:31 +0300

    change

--- AbstractUniversity/AbstractUniversityBusinessLogic/BindingModels/CourseBindingModel.cs
+++ AbstractUniversity/AbstractUniversityBusinessLogic/BindingModels/CourseBindingModel.cs
@@ -8,6 +8,8 @@ namespace AbstractUniversityBusinessLogic.BindingModels
     {
         public int? Id { get; set; }
         public string ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientLastName { get; set; }
         public int Price { get; set; }
         public DateTime DataCreate { get; set; }
         public bool IsReserved { get; set; }
