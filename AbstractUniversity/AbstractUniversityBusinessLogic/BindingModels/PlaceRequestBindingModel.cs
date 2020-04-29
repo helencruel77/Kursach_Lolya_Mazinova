f408a7d1904e4b0c4a5e8455945dcc28489d95c4
helencruel228@outlook.com <helencruel228@outlook.com>
2020-04-29 14:49:49 +0300

    add base models

---
+++ AbstractUniversity/AbstractUniversityBusinessLogic/BindingModels/PlaceRequestBindingModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractUniversityBusinessLogic.BindingModels
+{
+    public class PlaceRequestBindingModel
+    {
+    }
+}
---
+++ AbstractUniversity/AbstractUniversityBusinessLogic/Interfaces/IClientLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractUniversityBusinessLogic.Interfaces
+{
+    public interface IClientLogic
+    {
+    }
+}
