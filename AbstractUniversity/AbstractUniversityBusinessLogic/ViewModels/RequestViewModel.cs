5a6f563710368569a8f145ea600af5b6d16227c7
helga5622@gmail.com <helga5622@gmail.com>
2020-04-29 15:00:18 +0400

    proverka

--- AbstractUniversity/AbstractUniversityBusinessLogic/ViewModels/RequestViewModel.cs
+++ AbstractUniversity/AbstractUniversityBusinessLogic/ViewModels/RequestViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace AbstractUniversityBusinessLogic.ViewModels
 {
     public class RequestViewModel
     {
+        public int Id { get; set; }
+        [DisplayName("Название заявки")]
+        public string DisciplineName { get; set; }
+        public List<RequestPlaceViewModel> RequestPlace { get; set; }
     }
 }
