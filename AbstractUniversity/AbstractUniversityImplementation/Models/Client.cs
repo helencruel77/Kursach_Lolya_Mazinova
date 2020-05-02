63d6bcc51c47e92a6efdd3dccdaa8a513dad6739
helencruel228@outlook.com <helencruel228@outlook.com>
2020-05-02 10:16:28 +0300

    add models

---
+++ AbstractUniversity/AbstractUniversityImplementation/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace AbstractUniversityImplementation.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ClientName { get; set; }
+        [Required]
+        public string ClientLastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Login { get; set; }
+        public virtual Course Course { get; set; }
+    }
+}
---
+++ AbstractUniversity/AbstractUniversityImplementation/Models/Course.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace AbstractUniversityImplementation.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        [Required]
+        public string ClientName { get; set; }
+        [Required]
+        public string ClientLastName { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public DateTime DataCreate { get; set; }
+        [Required]
+        public bool IsReserved { get; set; }
+        [ForeignKey("ClientId")]
+        public virtual List<Client> Clients { get; set; }
+    }
+}
