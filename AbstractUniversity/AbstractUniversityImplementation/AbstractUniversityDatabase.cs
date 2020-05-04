31ae153873e40981c1fac21072a030aca4204a50
helencruel228@outlook.com <helencruel228@outlook.com>
2020-05-04 13:03:15 +0300

    add dbcontext

---
+++ AbstractUniversity/AbstractUniversityImplementation/AbstractUniversityDatabase.cs
@@ -0,0 +1,28 @@
+﻿using AbstractUniversityImplementation.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractUniversityImplementation
+{
+    public class AbstractUniversityDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-1L0DP37\SQLEXPRESS;Initial Catalog=AbstractUniversity;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Client> Clients { set; get; }
+        public virtual DbSet<Course> Courses { set; get; }
+        public virtual DbSet<Discipline> Disciplines { set; get; }
+        public virtual DbSet<DisciplineCourse> DisciplineCourses { set; get; }
+        public virtual DbSet<Place> Places { set; get; }
+        public virtual DbSet<PlaceDiscipline> PlaceDisciplines { set; get; }
+        public virtual DbSet<Request> Requests { set; get; }
+        public virtual DbSet<RequestPlace> RequestPlaces { set; get; }
+    }
+}
